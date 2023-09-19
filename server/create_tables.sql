78bd1f9cf501f3425d439f78480510044c3555fe
devid-corsard <truegrinde@gmail.com>
2023-09-19 17:23:19 +0300

    scripts: added files for running in docker

--- server/create_tables.sql
+++ server/create_tables.sql
@@ -1,12 +1,19 @@
 CREATE TABLE positions (
 	position_id BIGSERIAL NOT NULL PRIMARY KEY,
 	position VARCHAR(50) NOT NULL
-)
+);
+
+INSERT INTO positions (position) VALUES('Security');
+INSERT INTO positions (position) VALUES('Designer');
+INSERT INTO positions (position) VALUES('Content manager');
+INSERT INTO positions (position) VALUES('Lawyer');
 
 CREATE TABLE counter (
 	name VARCHAR(50) NOT NULL,
 	amount BIGINT NOT NULL DEFAULT 0
-)
+);
+
+INSERT INTO counter (name) VALUES('total_users');
 
 CREATE TABLE users (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
@@ -16,20 +23,9 @@ CREATE TABLE users (
 	position_id BIGINT NOT NULL REFERENCES positions(position_id),
 	photo VARCHAR(1000) NOT NULL,
 	registration_timestamp TIMESTAMP NOT NULL
-)
-
-INSERT INTO positions (position) VALUES('Security');
-INSERT INTO positions (position) VALUES('Designer');
-INSERT INTO positions (position) VALUES('Content manager');
-INSERT INTO positions (position) VALUES('Lawyer');
-
-SELECT * FROM users
-    ORDER BY registration_timestamp
-    LEFT JOIN positions
-    ON positions.id = users.position_id
+);
 
 CREATE TABLE token (
 	token_id BIGSERIAL NOT NULL PRIMARY KEY,
 	body VARCHAR(300) NOT NULL
-)
-
+);
---
+++ server/init_dirs.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/db_data/abz
+mkdir -p ../storage
